3bdb7b7f3ab131d30cb9b64cad60106263864b62
Wojtek <golewski.wojciech@outlook.com>
2019-11-15 14:59:24 +0100

    initial code for users-list component added

--- frontend/src/app/components/users-list/users-list.component.ts
+++ frontend/src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-users-list',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  users: string[] = [];
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
+    this.apiService.getUsers().subscribe(data => {
+      this.users = data;
+    })
   }
-
+  
 }
