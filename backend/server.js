c12fe03e775cf58ce579931fe8ecd22ebbe5d9b8
Wojtek <golewski.wojciech@outlook.com>
2019-11-15 05:53:39 +0100

    bodyParser implemented in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
 var posts = [
@@ -12,6 +13,7 @@ var posts = [
 ];
 
 app.use(cors());
+app.use(bodyParser.text());
 
 app.get('/posts', (req, res) => {
     res.send(posts);
