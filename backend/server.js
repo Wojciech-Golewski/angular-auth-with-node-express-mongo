d733a56b9daf3153ce3a53af649658a15011f7da
Wojtek <golewski.wojciech@outlook.com>
2019-11-15 06:30:14 +0100

    initial connection to an external MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
 var posts = [
@@ -26,6 +27,14 @@ app.post('/register', (req, res) => {
     // TODO: missing validation
 
     res.sendStatus(200);
-})
+});
+
+mongoose.connect(
+    'mongodb+srv://johnDoe:Password.1@angularauthtest-kmk3l.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (!err) console.log('connected to mongoDB');
+    }
+);
 
 app.listen(3000);
