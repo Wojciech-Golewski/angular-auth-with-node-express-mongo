17ae235bd4fa9112560fdbc03156a42c27bbeb78
Wojtek <golewski.wojciech@outlook.com>
2019-11-20 18:59:31 +0100

    receiving profile data and displaying it to profile.component

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +9,18 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile: User;
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService
+    ) { }
 
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.params.id;
+    this.apiService.getProfile(id).subscribe(data => {
+      this.profile = data;
+    })
   }
 
 }
