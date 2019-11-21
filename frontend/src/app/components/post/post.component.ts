5c821c50cd8b44d9715350c1f890aa6531d822de
Wojtek <golewski.wojciech@outlook.com>
2019-11-21 20:29:58 +0100

    added initial code for post.component

--- frontend/src/app/components/post/post.component.ts
+++ frontend/src/app/components/post/post.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(
+    private apiService: ApiService
+  ) { }
 
 }
