64202d853b7339de6c5538fc5961ebff27ea392c
Wojtek <golewski.wojciech@outlook.com>
2019-11-22 18:59:13 +0100

    assigning token dynamically to requests

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private injector: Injector) { }
+    
     intercept(req, next) {
+        var auth = this.injector.get(AuthService)
         var authRequest = req.clone({
-            headers: req.headers.set('Authorization', 'token ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWJqZWN0IjoiNWRkNTg2ODZiYjE0NzkzMjM0MTJlYjFhIn0.48NxWpCF3I9XR3kzCMonvdBiopONtjwwHE20K0K1aQM')
+            headers: req.headers.set('Authorization', 'token ' + auth.token)
         })
         return next.handle(authRequest);
     }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 
 const apiUrl: string = 'http://localhost:3000/auth/';
+const TOKEN_KEY: string = 'token';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ const apiUrl: string = 'http://localhost:3000/auth/';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+
+  get token() {
+    return localStorage.getItem(TOKEN_KEY);
+  }
 
   registerUser(registeredData: User) {
     this.http.post<User>(`${apiUrl}register`, registeredData)
@@ -22,7 +27,7 @@ export class AuthService {
     this.http.post<User>(`${apiUrl}login`, loginData)
     .subscribe(res => {
       console.log(res);
-      localStorage.setItem('token', JSON.parse(JSON.stringify(res)).token);
+      localStorage.setItem(TOKEN_KEY, JSON.parse(JSON.stringify(res)).token);
     })
   }
 }
