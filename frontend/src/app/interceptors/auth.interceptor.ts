b4ca4335ccad6042ff24c7e0294a24ba07b77c41
Wojtek <golewski.wojciech@outlook.com>
2019-11-22 18:49:09 +0100

    added hard-coded auth token to all the requests

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpInterceptor } from '@angular/common/http';
+import { HttpInterceptor } from '@angular/common/http';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req, next) {
-        return next.handle();
+        var authRequest = req.clone({
+            headers: req.headers.set('Authorization', 'token ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWJqZWN0IjoiNWRkNTg2ODZiYjE0NzkzMjM0MTJlYjFhIn0.48NxWpCF3I9XR3kzCMonvdBiopONtjwwHE20K0K1aQM')
+        })
+        return next.handle(authRequest);
     }
 
 }
